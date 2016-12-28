dc1e3005b59d5f645206bf407beb2690ec14b912
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-12-28 04:45:11 +0400

    Create synch.sv

---
+++ synch.sv
@@ -0,0 +1,13 @@
+/*
+ * Button press synchronizer
+ * out is only high for one clock cycle after in becomes high
+ */
+module synch(input logic clk, in,
+             output logic out);
+	logic prev = 0;
+    
+    always @(posedge clk) begin
+        prev <= in;
+        out <= in & !prev;
+    end
+endmodule
